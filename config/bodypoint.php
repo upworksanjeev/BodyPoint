3568bd456deea1c2d08618900c268d9babf6fe83
upworksanjeev <upworksanjeev@gmail.com>
2023-10-05 12:38:07 +0530

    Frontend login and dashboard section

---
+++ config/bodypoint.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'logo' => '/img/bodypoint.svg',
+
+];
---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,2 @@
+<img src="{{ asset(Config::get('bodypoint.logo')) }}" {{ $attributes->merge(['class' => '']) }} alt="Logo"
+    width="100%">
