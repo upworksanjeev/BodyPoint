def60ac3cc8223921f7a39b3c242dff44a5447d7
sanjeev <kam.2391@gmail.com>
2023-10-27 14:14:55 +0530

    Product Categories
    Product Categories in Admin Panel

---
+++ app/Nova/Category.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Image;
+use Laravel\Nova\Fields\Select;
+use Illuminate\Support\Facades\Storage;
+use App\Nova\Category;
+use Laravel\Nova\Fields\HasOne;
+
+class Category extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\Category>
+     */
+    public static $model = \App\Models\Category::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id','name','description'
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable(),
+			 Text::make('Name')
+                ->sortable()
+				->required(true)
+                ->rules('required', 'max:255'),
+			 Text::make('Description')
+                ->sortable()
+				->required(true)
+                ->rules('required', 'max:255'),
+			Image::make('Image')->disk('category_uploads')->disableDownload()->preview(function ($value, $disk) {
+					return $value
+                    ? Storage::disk($disk)->url($value)
+                    : null;
+			}),
+			
+			Select::make('Parent Category','parent_cat_id')->searchable()->options(\App\Models\Category::pluck('name', 'id')),
+			
+
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+}
