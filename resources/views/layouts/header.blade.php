41e8db192ca52cff8f9148e7a9176057a1985e44
sanjeev <kam.2391@gmail.com>
2023-11-28 19:33:15 +0530

    changes in header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,13 +5,13 @@
 </header>
 <header class="bg-[#008C99]">
       <div class="ctm-container mx-auto px-4 xl:px-8">
-        <div class="py-2 mb-5 text-sm border-b-[1px] hidden lg:flex">
+        <div class="py-2 mb-5 border-b-[1px] hidden lg:flex">
           <div
-            class="w-3/4 h-8 flex items-center text-sm xl:text-base text-[#fff]">
+            class="w-3/4 h-8 flex items-center text-[15px] text-[#fff] font-normal">
            
-            Welcome to bodypoint. We provides comfort and function of children and adults who use wheelchairs and other mobility devices.
+            Welcome to BodyPoint. We provides comfort and function of children and adults who use wheelchairs and other mobility devices.
           </div>
-          <div class="w-1/4 h-8 flex justify-end text-[#fff]">
+          <div class="w-1/4 h-8 flex justify-end text-[#fff] text-[15px] font-normal">
               <x-login-nav />
             
           </div>
