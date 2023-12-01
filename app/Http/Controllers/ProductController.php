464462d88ab53ddf9f537dae34af6746465f043b
sanjeev <kam.2391@gmail.com>
2023-12-01 18:14:45 +0530

    design changes for products in frontend

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,23 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ProfileUpdateRequest;
-use App\Http\Requests\StorePostRequest; 
-use Illuminate\Http\RedirectResponse;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 use App\Models\Category;
-use App\Models\ProductMedia;
 use App\Models\CategoryProduct;
 use App\Models\Product;
 use App\Models\Media;
-use App\Models\Country;
-use App\Models\UserDetails;
-use App\Models\User;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Contracts\Database\Eloquent\Builder;
+
 
 
 class ProductController extends Controller
