28b4dcad1db6a92762048388b90519e7a36f42eb
sanjeev <kam.2391@gmail.com>
2023-12-13 16:45:43 +0530

    Product overview and video link fields

--- app/Nova/Product.php
+++ app/Nova/Product.php
@@ -19,6 +19,10 @@ use Laravel\Nova\Fields\MorphToMany;
 use App\Models\Category;
 use Outl1ne\MultiselectField\Multiselect;
 use Ardenthq\ImageGalleryField\ImageGalleryField;
+use Emilianotisato\NovaTinyMCE\NovaTinyMCE;
+use Eminiarts\Tabs\Traits\HasTabs;
+use Eminiarts\Tabs\Tabs;
+use Eminiarts\Tabs\Tab;
 
 
 class Product extends Resource
@@ -56,35 +60,64 @@ class Product extends Resource
     {
 
         return [
-            ID::make()->sortable(),
-			Text::make('Name','name')->sortable()->required(true)->rules('required', 'max:255'),
-            Multiselect::make('Categories', 'categories')->belongsToMany(\App\Nova\Category::class, false),
-            ImageGalleryField::make('Images')
-                ->rules('mimes:jpeg,png,jpg,gif', 'dimensions:min_width=150,min_height=150', 'max:5000')
-                ->rulesMessages([
-                    'mimes'      => 'You must use a valid jpeg, png, jpg or gif image.',
-                    'max'        => 'The image must be less than 5MB.',
-                    'dimensions' => 'The image must be at least 150px wide and 150px tall.',
-                ])
-                ->help('Min size 150 x 150. Max filesize 5MB.')
-                ->showOnIndex(),
-			Text::make('Tagline','small_description')->maxlength(255)->hideFromIndex(),
-			Text::make('Tagline','small_description')->displayUsing(function($id) {
-				$part = strip_tags(substr($id, 0, 20));
-				return $part . "...";
-				})->onlyOnIndex(),
+            Tabs::make('Product', [
+                Tab::make('General Information', [
+                    ID::make()->sortable(),
+                    Text::make('Name','name')->sortable()->required(true)->rules('required', 'max:255'),
+                    Text::make('Tagline','small_description')->maxlength(255)->hideFromIndex(),
+                    Textarea::make('Description','description')->rows(3)->hideFromIndex(),
+                    Multiselect::make('Categories', 'categories')->belongsToMany(\App\Nova\Category::class, false),
+                    ImageGalleryField::make('Images')
+                        ->rules('mimes:jpeg,png,jpg,gif', 'dimensions:min_width=150,min_height=150', 'max:5000')
+                        ->rulesMessages([
+                            'mimes'      => 'You must use a valid jpeg, png, jpg or gif image.',
+                            'max'        => 'The image must be less than 5MB.',
+                            'dimensions' => 'The image must be at least 150px wide and 150px tall.',
+                        ])
+                        ->help('Min size 150 x 150. Max filesize 5MB.')
+                        ->showOnIndex(),
+					Text::make('Video Link','video')->maxlength(255)->hideFromIndex(),
+                    Text::make('Tagline','small_description')->displayUsing(function($id) {
+                        $part = strip_tags(substr($id, 0, 20));
+                        return $part . "...";
+                        })->onlyOnIndex(),
+						
+     				Text::make('Description','description')->displayUsing(function($id) {
+                        $part = strip_tags(substr($id, 0, 20));
+                        return $part . "...";
+                        })->onlyOnIndex(),
+						
+                  
+                ]),
+                Tab::make('Overview', [
+                    NovaTinyMCE::make('Overview','overview')->hideFromIndex()->alwaysShow()->options([
+                        'use_lfm' => true
+                        ]),
+                ]),  
+				Tab::make('Sizing', [
+                    NovaTinyMCE::make('Sizing','sizing')->hideFromIndex()->alwaysShow()->options([
+                        'use_lfm' => true
+                        ]),
+                ]),
+                Tab::make('Instruction of use', [
+                    NovaTinyMCE::make('Instruction of use','instruction_of_use')->hideFromIndex()->alwaysShow()->options([
+                        'use_lfm' => true
+                        ]),
+                ]),
+                Tab::make('Warranty', [
+                    NovaTinyMCE::make('Warranty','warranty')->hideFromIndex()->alwaysShow()->options([
+                        'use_lfm' => true
+                        ]),
+                ]),
+                Tab::make('Product Attributes', [
+                    Select::make('Product Type','product_type')->options([
+                        'Single' => 'Single',
+                        'Option' => 'Option',
+                    ]),
 
-			Trix::make('Description','description')->hideFromIndex()->alwaysShow(),
-			Trix::make('Sizing','sizing')->hideFromIndex()->alwaysShow(),
-			Trix::make('Instruction of use','instruction_of_use')->hideFromIndex()->alwaysShow(),
-			Trix::make('Warranty','warranty')->hideFromIndex()->alwaysShow(),
-
-			Select::make('Product Type','product_type')->options([
-				'Single' => 'Single',
-				'Option' => 'Option',
-			]),
-
-            HasMany::make('Attribute','attribute',\App\Nova\ProductAttribute::class)->hideFromIndex(),
+                    HasMany::make('Attribute','attribute',\App\Nova\ProductAttribute::class)->hideFromIndex(),
+                ]),
+            ]),
         ];
     }
 
