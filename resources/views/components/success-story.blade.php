de811ff78b1d79aee5b37eecdb807fead3d032b0
Sanjeev Kumar <upworksanjeev@gmail.com>
2024-04-03 15:31:25 +0530

    Apply New design success story section.

--- resources/views/components/success-story.blade.php
+++ resources/views/components/success-story.blade.php
@@ -1,5 +1,133 @@
 <section class="bg-[#f5f5f7]">
-	<div class="ctm-container py-[30px] md:py-[60px]">
-		<h2 class="text-[#333] text-[30px] font-[700]">Success Story</h2>
+	<div class="max-w-screen-xl mx-auto py-[30px] md:py-[60px] lg:px-0 px-5">
+		<div class="flex items-center justify-between mb-10 lg:flex-row flex-col">
+			<h2 class="text-[#333] text-[30px] font-[700] lg:mb-0">Success Story</h2>
+			<button class="bg-[#FE7300] rounded-lg text-white text-base font-medium min-w-[180px] p-4 lg:block hidden">Share Your Story</button>
+		</div>
+		<div class="grid lg:grid-cols-3 gap-6 lg:mb-0 mb-6">
+			<div>
+				<div class="rounded-[24px] relative overflow-hidden mb-6">
+					<img src="/img/image-1.png" alt="" class="w-full" />
+					<div class="absolute top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center">
+						<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+							<path d="M18.1818 14.7727V35.2273L35.2272 25L18.1818 14.7727Z" stroke="white" stroke-width="1.5" stroke-linejoin="round" />
+							<circle cx="25" cy="25" r="24" stroke="white" stroke-width="2" />
+						</svg>
+					</div>
+				</div>
+				<div>
+					<p class="text-base font-bold text-[#008C99] mb-3">Customer Story</p>
+					<h4 class="text-[24px] leading-8 font-bold text-black mb-6">Lorem Ipsum is simply dummy text
+						of the printing and industry. </h4>
+					<a href="#" class="text-sm font-light text-[#008C99] mb-3 flex items-center underline group">Read Customer Story <svg class="ms-2 translate-x-0 relative transition duration-150 ease-in-out group-hover:transition group-hover:duration-150 group-hover:ease-in-out group-hover:translate-x-2" xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 12 10" fill="none">
+							<path d="M11 5H1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+							<path d="M7 9L11 5L7 1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+						</svg></a>
+				</div>
+			</div>
+			<div>
+				<div class="rounded-[24px] relative overflow-hidden mb-6">
+					<img src="/img/image-1.png" alt="" class="w-full" />
+					<div class="absolute top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center">
+						<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+							<path d="M18.1818 14.7727V35.2273L35.2272 25L18.1818 14.7727Z" stroke="white" stroke-width="1.5" stroke-linejoin="round" />
+							<circle cx="25" cy="25" r="24" stroke="white" stroke-width="2" />
+						</svg>
+					</div>
+				</div>
+				<div>
+					<p class="text-base font-bold text-[#008C99] mb-3">Customer Story</p>
+					<h4 class="text-[24px] leading-8 font-bold text-black mb-6">Lorem Ipsum is simply dummy text
+						of the printing and industry. </h4>
+					<a href="#" class="text-sm font-light text-[#008C99] mb-3 flex items-center underline group">Read Customer Story <svg class="ms-2 translate-x-0 relative transition duration-150 ease-in-out group-hover:transition group-hover:duration-150 group-hover:ease-in-out group-hover:translate-x-2" xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 12 10" fill="none">
+							<path d="M11 5H1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+							<path d="M7 9L11 5L7 1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+						</svg></a>
+				</div>
+			</div>
+			<div>
+				<div class="rounded-[24px] relative overflow-hidden mb-6">
+					<img src="/img/image-1.png" alt="" class="w-full" />
+					<div class="absolute top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center">
+						<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+							<path d="M18.1818 14.7727V35.2273L35.2272 25L18.1818 14.7727Z" stroke="white" stroke-width="1.5" stroke-linejoin="round" />
+							<circle cx="25" cy="25" r="24" stroke="white" stroke-width="2" />
+						</svg>
+					</div>
+				</div>
+				<div>
+					<p class="text-base font-bold text-[#008C99] mb-3">Customer Story</p>
+					<h4 class="text-[24px] leading-8 font-bold text-black mb-6">Lorem Ipsum is simply dummy text
+						of the printing and industry. </h4>
+					<a href="#" class="text-sm font-light text-[#008C99] mb-3 flex items-center underline group">Read Customer Story <svg class="ms-2 translate-x-0 relative transition duration-150 ease-in-out group-hover:transition group-hover:duration-150 group-hover:ease-in-out group-hover:translate-x-2" xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 12 10" fill="none">
+							<path d="M11 5H1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+							<path d="M7 9L11 5L7 1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+						</svg></a>
+				</div>
+			</div>
+			<div>
+				<div class="rounded-[24px] relative overflow-hidden mb-6">
+					<img src="/img/image-1.png" alt="" class="w-full" />
+					<div class="absolute top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center">
+						<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+							<path d="M18.1818 14.7727V35.2273L35.2272 25L18.1818 14.7727Z" stroke="white" stroke-width="1.5" stroke-linejoin="round" />
+							<circle cx="25" cy="25" r="24" stroke="white" stroke-width="2" />
+						</svg>
+					</div>
+				</div>
+				<div>
+					<p class="text-base font-bold text-[#008C99] mb-3">Customer Story</p>
+					<h4 class="text-[24px] leading-8 font-bold text-black mb-6">Lorem Ipsum is simply dummy text
+						of the printing and industry. </h4>
+					<a href="#" class="text-sm font-light text-[#008C99] mb-3 flex items-center underline group">Read Customer Story <svg class="ms-2 translate-x-0 relative transition duration-150 ease-in-out group-hover:transition group-hover:duration-150 group-hover:ease-in-out group-hover:translate-x-2" xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 12 10" fill="none">
+							<path d="M11 5H1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+							<path d="M7 9L11 5L7 1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+						</svg></a>
+				</div>
+			</div>
+			<div>
+				<div class="rounded-[24px] relative overflow-hidden mb-6">
+					<img src="/img/image-1.png" alt="" class="w-full" />
+					<div class="absolute top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center">
+						<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+							<path d="M18.1818 14.7727V35.2273L35.2272 25L18.1818 14.7727Z" stroke="white" stroke-width="1.5" stroke-linejoin="round" />
+							<circle cx="25" cy="25" r="24" stroke="white" stroke-width="2" />
+						</svg>
+					</div>
+				</div>
+				<div>
+					<p class="text-base font-bold text-[#008C99] mb-3">Customer Story</p>
+					<h4 class="text-[24px] leading-8 font-bold text-black mb-6">Lorem Ipsum is simply dummy text
+						of the printing and industry. </h4>
+					<a href="#" class="text-sm font-light text-[#008C99] mb-3 flex items-center underline group">Read Customer Story <svg class="ms-2 translate-x-0 relative transition duration-150 ease-in-out group-hover:transition group-hover:duration-150 group-hover:ease-in-out group-hover:translate-x-2" xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 12 10" fill="none">
+							<path d="M11 5H1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+							<path d="M7 9L11 5L7 1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+						</svg></a>
+				</div>
+			</div>
+			<div>
+				<div class="rounded-[24px] relative overflow-hidden mb-6">
+					<img src="/img/image-1.png" alt="" class="w-full" />
+					<div class="absolute top-0 bottom-0 left-0 right-0 z-10 flex items-center justify-center">
+						<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
+							<path d="M18.1818 14.7727V35.2273L35.2272 25L18.1818 14.7727Z" stroke="white" stroke-width="1.5" stroke-linejoin="round" />
+							<circle cx="25" cy="25" r="24" stroke="white" stroke-width="2" />
+						</svg>
+					</div>
+				</div>
+				<div>
+					<p class="text-base font-bold text-[#008C99] mb-3">Customer Story</p>
+					<h4 class="text-[24px] leading-8 font-bold text-black mb-6">Lorem Ipsum is simply dummy text
+						of the printing and industry. </h4>
+					<a href="#" class="text-sm font-light text-[#008C99] mb-3 flex items-center underline group">Read Customer Story <svg class="ms-2 translate-x-0 relative transition duration-150 ease-in-out group-hover:transition group-hover:duration-150 group-hover:ease-in-out group-hover:translate-x-2" xmlns="http://www.w3.org/2000/svg" width="12" height="10" viewBox="0 0 12 10" fill="none">
+							<path d="M11 5H1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+							<path d="M7 9L11 5L7 1" stroke="#008C99" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+						</svg></a>
+				</div>
+			</div>
+		</div>
+		<div class="flex items-center justify-between mb-10 lg:flex-row flex-col">
+			<button class="bg-[#FE7300] rounded-lg text-white text-base font-medium lg:min-w-[180px] w-full p-4 lg:hidden block">Share Your Story</button>
+		</div>
 	</div>
 </section>
