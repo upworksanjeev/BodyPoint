08fdf843a1dce81118e6bd65f4734ee7af4c8cec
upworkSanjeev <upworksanjeev@gmail.com>
2024-09-19 18:20:48 +0530

    products prices fetch according to sku

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CategoryController;
@@ -38,26 +39,26 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-	Route::post('/cart', [CartController::class, 'index'])->name('cart.save');
-	Route::get('/cart', [CartController::class, 'quickEntry'])->name('cart');
-	Route::post('/update-cart-item', [CartController::class, 'updateCartItem'])->name('update-cart-item');
-	Route::post('/update-cart-item-marked', [CartController::class, 'updateCartItemMarked'])->name('update-cart-item-marked');
-	Route::post('/delete-cart', [CartController::class, 'deleteCart'])->name('delete-cart');
-	Route::post('/search-product', [CartController::class, 'searchProduct'])->name('search-product');
-	Route::post('/add-to-cart', [CartController::class, 'addToCart'])->name('add-to-cart');
-	Route::get('/get-cart-count', [CartController::class, 'getCartCount'])->name('get-cart-count');
-	Route::get('/checkout', [CheckoutController::class, 'checkout'])->name('checkout');
-	Route::get('/quote', [CheckoutController::class, 'quote'])->name('quote');
-	Route::post('/confirm-order', [CheckoutController::class, 'saveOrder'])->name('confirm-order');
-	Route::get('/order', [CheckoutController::class, 'myOrder'])->name('order');
-	Route::post('/order', [CheckoutController::class, 'myOrder'])->name('order-search');
-	Route::get('/payment', [CheckoutController::class, 'payment'])->name('payment');
-	Route::get('/shipping', [CheckoutController::class, 'index'])->name('shipping');
-	Route::get('/pdf', [CheckoutController::class, 'pdfDownload'])->name('pdf-download-get');
-	Route::post('/pdf', [CheckoutController::class, 'pdfDownload'])->name('pdf-download');
-	Route::post('/receipt-download', [CheckoutController::class, 'receiptDownload'])->name('receipt-download');
-	Route::post('/update-purchase-no', [CheckoutController::class, 'updatePurchaseNo'])->name('update-purchase-no');
-	Route::post('/add-success-story', [ProductController::class, 'addStory'])->name('add-success-story');
+    Route::post('/cart', [CartController::class, 'index'])->name('cart.save');
+    Route::get('/cart', [CartController::class, 'quickEntry'])->name('cart');
+    Route::post('/update-cart-item', [CartController::class, 'updateCartItem'])->name('update-cart-item');
+    Route::post('/update-cart-item-marked', [CartController::class, 'updateCartItemMarked'])->name('update-cart-item-marked');
+    Route::post('/delete-cart', [CartController::class, 'deleteCart'])->name('delete-cart');
+    Route::post('/search-product', [CartController::class, 'searchProduct'])->name('search-product');
+    Route::post('/add-to-cart', [CartController::class, 'addToCart'])->name('add-to-cart');
+    Route::get('/get-cart-count', [CartController::class, 'getCartCount'])->name('get-cart-count');
+    Route::get('/checkout', [CheckoutController::class, 'checkout'])->name('checkout');
+    Route::get('/quote', [CheckoutController::class, 'quote'])->name('quote');
+    Route::post('/confirm-order', [CheckoutController::class, 'saveOrder'])->name('confirm-order');
+    Route::get('/order', [CheckoutController::class, 'myOrder'])->name('order');
+    Route::post('/order', [CheckoutController::class, 'myOrder'])->name('order-search');
+    Route::get('/payment', [CheckoutController::class, 'payment'])->name('payment');
+    Route::get('/shipping', [CheckoutController::class, 'index'])->name('shipping');
+    Route::get('/pdf', [CheckoutController::class, 'pdfDownload'])->name('pdf-download-get');
+    Route::post('/pdf', [CheckoutController::class, 'pdfDownload'])->name('pdf-download');
+    Route::post('/receipt-download', [CheckoutController::class, 'receiptDownload'])->name('receipt-download');
+    Route::post('/update-purchase-no', [CheckoutController::class, 'updatePurchaseNo'])->name('update-purchase-no');
+    Route::post('/add-success-story', [ProductController::class, 'addStory'])->name('add-success-story');
     Route::get('/list-stock', function () {
         $syspro_service = new SysproService();
         $url = 'ListStock';
@@ -70,12 +71,6 @@ Route::middleware('auth')->group(function () {
         $response = $syspro_service->placeQuote($url);
         dd($response['response']);
     });
-    Route::get('/update-product-prices', function () {
-        $syspro_service = new SysproService();
-        $url = 'GetCustomerDetails/'.auth()->user()->customer_id;
-        $response = $syspro_service->getCustomerDetails($url);
-        dd($response['response']);
-    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
