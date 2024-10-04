64186b8f90c8029034598166e360b196167c2f6e
upworkSanjeev <upworksanjeev@gmail.com>
2024-10-04 10:49:03 +0530

    formatting code and orders shwon accordig to user

--- resources/views/components/cart/order-list.blade.php
+++ resources/views/components/cart/order-list.blade.php
@@ -1,98 +1,96 @@
-
 <div id="accordion-collapse" data-accordion="collapse">
+    @if(isset($order[0]))
+        @foreach ($order as $orders)
+            <h2 id="accordion-collapse-heading-{{ $orders['id'] }}" class="">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-[16px] focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-{{ $orders['id'] }}" aria-expanded="true" aria-controls="accordion-collapse-body-{{ $orders['id'] }}">
+                    <span>Order No: {{ $orders['purchase_order_no'] }}</span>
+                    @if(!empty($orders['status']))
+                    <span>Order Status: {{ $orders->order_status }}</span>
+                    @endif
+                    <!--x-icons.down-arrow /-->
+                    <span class="text-center">Order Date: {{ date('F j, Y',strtotime($orders['created_at'])) }}</span>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-{{ $orders['id'] }}" class="hidden" aria-labelledby="accordion-collapse-heading-{{ $orders['id'] }}">
+                <div class="p-5 border border-b-0 border-gray-200">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+                        <thead class="bg-[#00838f] font-semibold text-sm text-white">
+                            <tr>
+                                <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
+                                    Product name
+                                </th>
+                                <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
+                                    Stock Code
+                                </th>
+                                <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
+                                    Marked For
+                                </th>
+                                <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
+                                    Qty.
+                                </th>
+                                <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
+                                    Net Price
+                                </th>
+                                <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
+                                    Unit
+                                </th>
+                                <th scope="col" class="px-4 py-3 font-bold">
+                                    Total
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                                $subtotal = 0;
+                                $tax = 0.00;
+                            @endphp
+                            @foreach ($orders['OrderItem'] as $cartitem)
+                                <tr class="odd:bg-white even:bg-gray-50 border-b">
+                                    <td class="px-4 py-4 text-sm leading-[18px] text-[#3E3E3E] whitespace-nowrap border-e">
+                                        {{ $cartitem['Product']['name'] }}
+                                    </td>
+                                    <td class="px-4 py-4 text-[13px] leading-[18px] text-[#000] border-e">
+                                        {{ $cartitem['sku'] }}
+                                    </td>
+                                    <td class="px-4 py-4 text-[13px] leading-[18px] text-[#000] border-e">
+                                        {{ $cartitem['marked_for'] }}
+                                    </td>
+                                    <td class="px-4 py-4 text-[13px] leading-[18px] text-[#000] border-e">
+                                        {{ $cartitem['quantity'] }}
+                                    </td>
 
- @if(isset($order[0]))
-       @foreach ($order as $orders)
-  <h2 id="accordion-collapse-heading-{{ $orders['id'] }}" class="">
+                                    <td class="px-4 py-4 text-[13px] leading-[18px] text-[#000] border-e">
+                                        ${{ $cartitem['discount_price']?number_format($cartitem['discount_price'], 2, '.', ','):0 }}
+                                    </td>
 
-    <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-[16px] focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-{{ $orders['id'] }}" aria-expanded="true" aria-controls="accordion-collapse-body-{{ $orders['id'] }}">
+                                    <td class="px-4 py-4 text-[13px] leading-[18px] text-[#000] border-e">
+                                        EA
+                                    </td>
 
-      <span>Order No: {{ $orders['purchase_order_no'] }}</span>
-      @if(!empty($orders['status']))
-        <span>Order Status: {{ $orders->order_status }}</span>
-      @endif
-	  <!--x-icons.down-arrow /-->
-	  <span class="text-center">Order Date: {{ date('F j, Y',strtotime($orders['created_at'])) }}</span>
-    </button>
-  </h2>
-  <div id="accordion-collapse-body-{{ $orders['id'] }}" class="hidden" aria-labelledby="accordion-collapse-heading-{{ $orders['id'] }}">
-    <div class="p-5 border border-b-0 border-gray-200">
-      <table class="w-full text-sm text-left rtl:text-right text-gray-500">
-         <thead class="bg-[#00838f] font-semibold text-sm text-white">
-           <tr>
-             <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
-               Product name
-             </th>
-             <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
-               Stock Code
-             </th>
-			 <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
-               Marked For
-             </th>
-             <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
-               Qty.
-             </th>
-             <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
-               Net Price
-             </th>
-             <th scope="col" class="px-4 py-3 font-bold border-e border-gray-500">
-               Unit
-             </th>
-             <th scope="col" class="px-4 py-3 font-bold">
-               Total
-             </th>
-           </tr>
-         </thead>
-         <tbody>
-           <?php $subtotal = 0;
-            $tax = 0.00; ?>
-           @foreach ($orders['OrderItem'] as $cartitem)
-           <tr class="odd:bg-white even:bg-gray-50 border-b">
-             <td class="px-4 py-4 text-sm leading-[18px] text-[#3E3E3E] whitespace-nowrap border-e">
-               {{ $cartitem['Product']['name'] }}
-             </td>
-             <td class="px-4 py-4 text-[13px] leading-[18px] text-[#000] border-e">
-               {{ $cartitem['sku'] }}
-             </td>
-			 <td class="px-4 py-4 text-[13px] leading-[18px] text-[#000] border-e">
-               {{ $cartitem['marked_for'] }}
-             </td>
-             <td class="px-4 py-4 text-[13px] leading-[18px] text-[#000] border-e">
-               {{ $cartitem['quantity'] }}
-             </td>
-
-             <td class="px-4 py-4 text-[13px] leading-[18px] text-[#000] border-e">
-               ${{ $cartitem['discount_price']?number_format($cartitem['discount_price'], 2, '.', ','):0 }}
-
-             </td>
-
-             <td class="px-4 py-4 text-[13px] leading-[18px] text-[#000] border-e">
-               EA
-             </td>
-             <td class="px-4 py-4 text-[13px] font-bold leading-[18px] text-[#000]">
-               ${{ $cartitem['discount_price']?number_format($cartitem['discount_price']*$cartitem['quantity'], 2, '.', ','):0 }}
-             </td>
-           </tr>
-           <?php $subtotal += $cartitem['discount_price'] * $cartitem['quantity']; ?>
-           @endforeach
-           <tr class="odd:bg-white even:bg-gray-50 border-b">
-             <td class="w-4 p-4" colspan="2">
-				<div class="text-left"><span>BP Number: {{ $orders['bp_number'] }}</span></div>
-             </td>
-             <td class="w-4 p-4" colspan="8">
-
-               <div class="text-right">
-                 <h3 class="text-2xl font-normal text-[#000]" id="subtotal"><span class="font-bold">Subtotal:</span> ${{ number_format($subtotal, 2, '.', ',') }}</h3>
-               </div>
-             </td>
-           </tr>
-
-         </tbody>
-       </table>
-
-    </div>
-  </div>
-    @endforeach
-       @endif
-	     </div>
-
+                                    <td class="px-4 py-4 text-[13px] font-bold leading-[18px] text-[#000]">
+                                        ${{ $cartitem['discount_price']?number_format($cartitem['discount_price']*$cartitem['quantity'], 2, '.', ','):0 }}
+                                    </td>
+                                </tr>
+                                @php
+                                    $subtotal += $cartitem['discount_price'] * $cartitem['quantity'];
+                                @endphp
+                            @endforeach
+                            <tr class="odd:bg-white even:bg-gray-50 border-b">
+                                <td class="w-4 p-4" colspan="2">
+                                    <div class="text-left"><span>BP Number: {{ $orders['bp_number'] }}</span></div>
+                                </td>
+                                <td class="w-4 p-4" colspan="8">
+                                    <div class="text-right">
+                                        <h3 class="text-2xl font-normal text-[#000]" id="subtotal"><span class="font-bold">Subtotal:</span> ${{ number_format($subtotal, 2, '.', ',') }}</h3>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        @endforeach
+    @else
+        <h2>No Orders Found</h2>
+    @endif
+</div>
--- resources/views/order.blade.php
+++ resources/views/order.blade.php
@@ -1,63 +1,57 @@
 <x-mainpage-layout>
-
-  <x-cart-nav />
-  <section class="bg-[#F6F6F6] py-9">
-    <header>
-      <h2 class="text-lg text-[#00838f] font-bold text-center">
-        All Orders
-      </h2>
-
-      <p class="mt-1 text-sm text-gray-600 text-center">
-        List of all your orders
-      </p>
-    </header>
-    <div class="container mx-auto mt-9">
-      <div class="max-w-screen-xl mx-auto">
-	    
-	<form action="{{ route('order-search') }}" method="post">
-        <input type="hidden" value="<?= csrf_token() ?>" name="_token">
-
-		<div class="grid gap-6 mb-6 md:grid-cols-3">
-			<div>
-				<label for="search" class="block mb-2 text-sm font-medium text-gray-900">Search By:</label>
-				<input type="text" id="search_input" name="search_input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Order No/ BP Number" value="{{ $search??'' }}" />
-			</div>
-			<div>
-				<label for="start_date" class="block mb-2 text-sm font-medium text-gray-900">Order Date: </label>
-				
-			<div date-rangepicker class="flex items-center">
-			  <div class="relative">
-				<div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-					 <x-icons.date />
-				</div>
-				<input name="start_date" id="start_date" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5" placeholder="Select date start" value="{{ $start_date??'' }}">
-			  </div>
-			  <span class="mx-4 text-gray-500">to</span>
-			  <div class="relative">
-				<div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-					<x-icons.date />
-				</div>
-				<input name="end_date" id="end_date" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5" placeholder="Select date end" value="{{ $end_date??'' }}">
-			</div>
-			</div>
-
-			</div>
-			<div class="flex items-center mt-5 gap-2">
-			
-			<button type="submit" name="search_order" class="py-2.5 px-5 text-sm font-medium text-white focus:outline-none bg-[#FF9119] rounded-full border border-[#FF9119] focus:z-10 focus:ring-4 focus:ring-[#FF9119]/40 flex gap-3 hover:bg-[#FF9119]/80 justify-center w-[160px] items-left">Search Order</button>
-			<a href="{{ route('order') }}" class="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-[#000000] hover:bg-[#00838f] hover:border-[#027480] hover:text-[#fff] focus:z-10 focus:ring-4 focus:ring-gray-100 flex gap-3 items-right justify-center w-[160px]">Clear Search</a>
-			 <button  type="submit" name="download" class="py-2.5 px-5 text-sm font-medium text-white focus:outline-none bg-[#FF9119] rounded-full border border-[#FF9119] focus:z-10 focus:ring-4 focus:ring-[#FF9119]/40 flex gap-3 items-center hover:bg-[#FF9119]/80 justify-center w-[160px]">Download</button>
-			</div>
-			</div>
-	</form>
-        <div class="relative overflow-x-auto sm:rounded-2xl mt-10" id="order_list">
-          <x-cart.order-list :order="$order" />
+    <x-cart-nav />
+    <section class="bg-[#F6F6F6] py-9">
+        <header>
+            <h2 class="text-lg text-[#00838f] font-bold text-center">
+                All Orders
+            </h2>
+            <p class="mt-1 text-sm text-gray-600 text-center">
+                List of all your orders
+            </p>
+        </header>
+        <div class="container mx-auto mt-9">
+            <div class="max-w-screen-xl mx-auto">
+                <form action="{{ route('order-search') }}" method="post">
+                    <input type="hidden" value="<?= csrf_token() ?>" name="_token">
+                    <div class="grid gap-6 mb-6 md:grid-cols-3">
+                        <div>
+                            <label for="search" class="block mb-2 text-sm font-medium text-gray-900">Search By:</label>
+                            <input type="text" id="search_input" name="search_input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Order No/ BP Number" value="{{ $search??'' }}" />
+                        </div>
+                        <div>
+                            <label for="start_date" class="block mb-2 text-sm font-medium text-gray-900">Order Date: </label>
+                            <div date-rangepicker class="flex items-center">
+                                <div class="relative">
+                                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                        <x-icons.date />
+                                    </div>
+                                    <input name="start_date" id="start_date" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5" placeholder="Select date start" value="{{ $start_date??'' }}">
+                                </div>
+                                <span class="mx-4 text-gray-500">to</span>
+                                <div class="relative">
+                                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                        <x-icons.date />
+                                    </div>
+                                    <input name="end_date" id="end_date" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5" placeholder="Select date end" value="{{ $end_date??'' }}">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex items-center mt-5 gap-2">
+                            <button type="submit" name="search_order" class="py-2.5 px-5 text-sm font-medium text-white focus:outline-none bg-[#FF9119] rounded-full border border-[#FF9119] focus:z-10 focus:ring-4 focus:ring-[#FF9119]/40 flex gap-3 hover:bg-[#FF9119]/80 justify-center w-[160px] items-left">Search Order</button>
+                            <a href="{{ route('order') }}" class="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-[#000000] hover:bg-[#00838f] hover:border-[#027480] hover:text-[#fff] focus:z-10 focus:ring-4 focus:ring-gray-100 flex gap-3 items-right justify-center w-[160px]">Clear Search</a>
+                            <button type="submit" name="download" class="py-2.5 px-5 text-sm font-medium text-white focus:outline-none bg-[#FF9119] rounded-full border border-[#FF9119] focus:z-10 focus:ring-4 focus:ring-[#FF9119]/40 flex gap-3 items-center hover:bg-[#FF9119]/80 justify-center w-[160px]">Download</button>
+                        </div>
+                    </div>
+                </form>
+                <div class="relative overflow-x-auto sm:rounded-2xl mt-10" id="order_list">
+                    <x-cart.order-list :order="$order" />
+                </div>
+                <div class="py-6 text-right">
+                    <a href="{{ route('home') }}" class="text-base text-[#00707B] font-normal leading-[18px] flex items-center justify-end gap-2">Continue Shopping
+                        <x-icons.next-arrow />
+                    </a>
+                </div>
+            </div>
         </div>
-        <div class="py-6 text-right">
-          <a href="{{ route('home') }}" class="text-base text-[#00707B] font-normal leading-[18px] flex items-center justify-end gap-2">Continue Shopping <x-icons.next-arrow /></a>
-        </div>
-
-      </div>
-    </div>
-  </section>
+    </section>
 </x-mainpage-layout>
