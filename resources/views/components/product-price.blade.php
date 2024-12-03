4afeb9cb1844ae3c439ed2e59ca50b5b8b099c1b
upworkSanjeev <upworksanjeev@gmail.com>
2024-12-03 10:46:50 +0530

    add to cart only for customers with customer class W WX WS

--- resources/views/components/product-price.blade.php
+++ resources/views/components/product-price.blade.php
@@ -10,7 +10,7 @@
 					</div>
 					<div class="ctm-price mt-[30px]">
                                 <div class="left-price">
-								
+
                                     <p class="text-[14px] text-[#6A6D73]">MSRP</p>
                                     <h6 class="text-[16px] text-[#000] font-[500]">YOUR PRICE</h6>
 									 @if ($product['discount']>0)
@@ -27,16 +27,17 @@
 										<p class="text-[14px] text-[#6A6D73]">${{ $product['discount_in_price'] ?? '' }} ({{ number_format($product['discount'], 2, '.', ',')  }}% Off)</p>
 										@endif
                                     </div>
-									 
+
 									<input type="hidden" name="variation_id" id="variation_id" value="{{ $product['variation_id'] ?? '' }}">
 									<input type="hidden" name="price" id="price" value="{{ $product['price'] ?? '' }}">
 									<input type="hidden" name="sku" id="sku" value="{{ $product['sku'] ?? '' }}">
 									<input type="hidden" name="msrp" id="msrp" value="{{ $product['msrp'] ?? '' }}">
 									<input type="hidden" name="discount_price" id="discount_price" value="{{ $product['discount_price'] ?? '' }}">
 									<input type="hidden" name="discount" id="discount" value="{{ $product['discount'] ?? '' }}">
-									
-									
-                                    <button type="submit" class="py-2.5 px-5 text-sm font-medium text-white focus:outline-none bg-[#FF9119] rounded-full border border-[#FF9119] focus:z-10 focus:ring-4 focus:ring-[#FF9119]/40 flex gap-3 items-center hover:bg-[#FF9119]/80 justify-center w-[160px]">Add To Cart</button>
-								
+
+                                    @if(in_array(getCustomerClass(), ['W', 'WX', 'WS']))
+                                        <button type="submit" class="py-2.5 px-5 text-sm font-medium text-white focus:outline-none bg-[#FF9119] rounded-full border border-[#FF9119] focus:z-10 focus:ring-4 focus:ring-[#FF9119]/40 flex gap-3 items-center hover:bg-[#FF9119]/80 justify-center w-[160px]">Add To Cart</button>
+                                    @endif
+
                                 </div>
                             </div>
