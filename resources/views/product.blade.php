b96423a9ae13e4a23e50feaafe7a9ce293b33de4
upworksanjeev <upworksanjeev@gmail.com>
2025-01-07 12:17:25 +0530

    Update product.blade.php

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -67,7 +67,7 @@
                                     <div class="detactor">
                                         <div class="detactor-left">
                                             @if($showFindPartnerButton ?? false)
-                                             <p class="text-[#000] flex items-center gap-[10px]"><i class="fas fa-map-marker-alt text-[20px]"></i> <span class="btn text-[18px]"><a href="{{$partnerPageURl}}"  target="_blank"><button class="border border-[#373B3C] text-[#373B3C] py-[8px] px-[30px]" >Find a Partner </button></a></span></p>
+                                             <p class="text-[#000] flex items-center gap-[10px]"><i class="fas fa-map-marker-alt text-[20px]"></i> <span class="btn text-[18px]"><a href="{{$partnerPageURl}}"  target="_blank"><button class="bg-[#FE7300] rounded-lg text-white text-base font-medium min-w-[180px] p-4 lg:block hidden" >Find a Partner </button></a></span></p>
                                             @endif
                                         </div>
                                         <div class="detactor-right">
