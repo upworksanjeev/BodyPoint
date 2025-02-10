b205b5b509467e547888dc2a330a307542d72a3c
sanjeev kumar <upworksanjeev@gmail.com>
2025-02-10 18:05:33 +0530

    design updated

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -2,9 +2,9 @@
 <section class="py-3">
 	<div class="ctm-container">
 	<?php if(isset($error)){ echo $error; }else{ ?>
-		<div class="ctm-prd-one relative overflow-hidden py-[30px] lg:py-[50px] px-[20px] lg:px-[70px] bg-no-repeat bg-center bg-cover h-[auto] md:h-[400px] flex justify-center rounded-[20px] after:content-['*'] after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 after:bg-[#000]/[.5] after:w-full after:h-full" style="background-image: url('<?php echo url('storage/' . $category['image']); ?>')">
+		<div class="ctm-prd-one relative overflow-hidden py-[30px] lg:py-[50px] px-[20px] lg:px-[70px] bg-no-repeat bg-center bg-cover h-[200px] md:h-[400px] flex justify-center rounded-[20px] after:content-['*'] after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 after:bg-[#000]/[.5] after:w-full after:h-full" style="background-image: url('<?php echo url('storage/' . $category['image']); ?>')">
                 <img src="{{ asset('img/small-logo.png') }}" class="h-[55px] max-w-[55px] contain absolute right-[10px] top-[10px] z-[1]" alt="">
-                <h3 class="text-[#fff] text-[25px] md:text-[52px] uppercase font-[800] leading-[39px] md:leading-[62px] z-[1]">{{ $category['name'] ?? '' }}</h3>
+                <h3 class="text-[#fff] text-[25px] md:text-[52px] uppercase font-[800] leading-[32px] md:leading-[62px] z-[1]">{{ $category['name'] ?? '' }}</h3>
                 <div class="mt-[10px] text-[18px] text-[#fff] max-w-[670px] z-[1]"><?php echo htmlspecialchars_decode(htmlspecialchars($category['description'])); ?></div>
         </div>
         <div class="prodct-Category my-[15px] md:my-[30px]">
