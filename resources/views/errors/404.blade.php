9d18bb8875d88cad66fdb8d93889e5baf46b1cd1
Sanjeev Kumar <upworksanjeev@gmail.com>
2025-02-27 16:36:44 +0530

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -8,15 +8,16 @@
                 <!-- Description -->
                 <p class="text-lg text-center text-gray-600  leading-relaxed">
                     We are sorry. But the page you are looking for is not available.<br>
-                    Perhaps you can try a new search.
+                   
                 </p>
             
                 <!-- Search Form -->
-                {{-- <form action="{{ route('product-search') }}" method="post" id="formsearch1" class="w-full max-w-md">
-                    @csrf
+                {{-- <form action="{{ route('product-search') }}" method="get" id="formsearch1" class="w-full max-w-md">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
                     <div class="relative flex items-center max-w-xs mx-auto w-full h-12  border ">
                         <input class="peer h-full w-full outline-none border-none focus:ring-0 text-sm text-gray-700 bg-[#f8f8f8]" 
-                            type="text" id="searchinput" name="searchinput" placeholder="Search..." />
+                            type="text" id="searchinput1" name="searchinput" placeholder="Search..." />
                         <div style="right: 16px;" class="absolute right-4 grid place-items-center h-full cursor-pointer text-gray-500" id="searchicon">
                             <x-icons.search class="w-5 h-5"/>
                         </div>
