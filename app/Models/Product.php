98d9210fc79e6bbd432da25c46c5d3a2151b374f
sanjeev kumar <upworksanjeev@gmail.com>
2025-07-11 16:04:40 +0530

    3D Image Experiences: Laravel Field Needed

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -16,25 +16,41 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model implements HasMedia
 {
-    use HasFactory, InteractsWithMedia , SoftDeletes;
+    use HasFactory, InteractsWithMedia, SoftDeletes;
 
-	 protected $fillable = [
-        'name','item_name','slug', 'sku', 'msrp', 'price', 'discount', 'product_type','small_description','description','overview','sizing','instruction_of_use','warranty','video'
+    protected $fillable = [
+        'name',
+        'item_name',
+        'slug',
+        'sku',
+        'msrp',
+        'price',
+        'discount',
+        'product_type',
+        'small_description',
+        'description',
+        'overview',
+        'sizing',
+        'instruction_of_use',
+        'warranty',
+        'video',
+        '3d_image_link '
     ];
-	protected $attributes = [
-       'product_type' => 'Single',
-     
+    protected $attributes = [
+        'product_type' => 'Single',
+
     ];
 
-	protected static function boot() {
+    protected static function boot()
+    {
         parent::boot();
 
         static::creating(function ($question) {
             $question->slug = Str::slug($question->name);
         });
     }
-	
-	public function attribute()
+
+    public function attribute()
     {
         return $this->hasMany(ProductAttribute::class, 'prod_id', 'id');
     }
@@ -49,14 +65,13 @@ class Product extends Model implements HasMedia
         $this->addMediaCollection('product-image');
     }
 
-	public function variation()
+    public function variation()
     {
         return $this->hasMany(Variation::class);
     }
-	
-	public function successStory()
+
+    public function successStory()
     {
         return $this->hasMany(SuccessStory::class);
     }
-
 }
