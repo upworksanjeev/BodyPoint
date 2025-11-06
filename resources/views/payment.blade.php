04b7e0e9e93d2e46dd7ee3074570fc1ce752fe9b
Sanjeev <upworksanjeev@gmail.com>
2025-11-06 18:42:36 +0530

    Card Changes

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -87,7 +87,7 @@
             </div>
 
 			<div>
-            <a onclick="addToCart()" class="py-2.5 px-5 text-sm font-medium text-white focus:outline-none bg-[#FF9119] rounded-full border border-[#FF9119] focus:z-10 focus:ring-4 focus:ring-[#FF9119]/40 flex gap-3 items-center hover:bg-[#FF9119]/80 justify-center w-[160px] float-right" href="{{ route('checkout') }}">
+            <a onclick="proceedToCheckout()" class="py-2.5 px-5 text-sm font-medium text-white focus:outline-none bg-[#FF9119] rounded-full border border-[#FF9119] focus:z-10 focus:ring-4 focus:ring-[#FF9119]/40 flex gap-3 items-center hover:bg-[#FF9119]/80 justify-center w-[160px] float-right" href="{{ route('checkout') }}">
               Next
             </a>
           </div>
